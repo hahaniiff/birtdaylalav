8e72331439f54d55f5d48f5a080cbbbeaa279d20
hahaniiff <150938693+hahaniiff@users.noreply.github.com>
2024-07-29 21:56:05 +0700

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -264,6 +264,15 @@ const animationTimeline = () => {
         },
         "+=1"
     );
+    function ScrollDiv(){
+
+        if(document.getElementById('ecran').scrollTop<(document.getElementById('ecran').scrollHeight-document.getElementById('ecran').offsetHeight)){-1
+              document.getElementById('ecran').scrollTop=document.getElementById('ecran').scrollTop+1
+              }
+        else {document.getElementById('ecran').scrollTop=0;}
+     }
+     
+     setInterval(ScrollDiv,50)
 
     // Restart Animation on click
     const replyBtn = document.getElementById("replay");
